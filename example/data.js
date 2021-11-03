c5d7aba23decfe5f0bdc95915670489c0cdad530
chengyuxing <chengyuxingo@gmail.com>
2021-11-03 11:29:26 +0800

    分页控件构建

---
+++ example/data.js
@@ -0,0 +1,155 @@
+const SOURCE = [{
+    tag: ['java', '开发工具'],
+    url: './file/ideaIU-2019.3.3.exe',
+    title: 'IntelliJ IDEA 2019.3.3',
+    content: `<span>最受欢迎且最智能的JAVA开发软件。</span><br>
+                    <span>插件:</span>
+                    <a href="./file/scala-intellij-bin-2019.3.23.zip" target="_blank">scala-intellij</a>,&nbsp;
+                    <a href="./file/lombok-plugin-0.28-2019.3.zip" target="_blank">lombok</a>
+                    `
+}, {
+    tag: ['数据库', '开发工具'],
+    url: './file/datagrip-2019.3.2.exe',
+    title: 'DataGrip 2019.3.2',
+    content: 'JetBrain家族的数据库管理软件。'
+}, {
+    tag: ['js', 'html', 'css', 'web', '开发工具'],
+    url: './file/WebStorm-2019.3.2.exe',
+    title: 'WebStorm 2019.3.2',
+    content: 'JetBrain家族的最智能的前端开发软件。'
+}, {
+    tag: ['python', '开发工具'],
+    url: './file/pycharm-professional-2019.3.3.exe',
+    title: 'Pycharm 2019.3.3',
+    content: 'JetBrain家族的Python开发软件。'
+}, {
+    tag: ['激活码', '密钥'],
+    url: './file/IYXUFLIR9J.txt',
+    title: 'License-V2019.3',
+    content: `<b style='color:green'>JetBrain全家桶2019.3版永久激活码。</b>`
+}, {
+    tag: ['数据库', '工具', 'excel'],
+    url: './file/DataLoader.exe',
+    title: 'DataLoader V3.0.0.beta',
+    content: `这是一个轻量级的数据导入导出工具，数据库方面支持<span style="font-weight:bold;">Oracle</span>，<span style='font-weight:bold'>PostgreSQL</span>，数据的导入与导出所支持的文件类型均为Excel，此工具为<span style='font-weight:bold;color:red;'>测试版</span>，使用过程中不可避免触发一些bug，欢迎提出，将会在下一个版本进行修复！`
+}, {
+    tag: ['java', '服务器'],
+    url: './file/apache-tomcat-9.0.30-windows-x86.zip',
+    title: 'Tomcat 9.0.30 windows版32位',
+    content: 'JAVA web 服务器'
+}, {
+    tag: ['java', '服务器'],
+    url: './file/apache-tomcat-9.0.30-windows-x64.zip',
+    title: 'Tomcat 9.0.30 windows版64位',
+    content: 'JAVA web 服务器'
+}, {
+    tag: ['java', '服务器'],
+    url: './file/apache-tomcat-9.0.31.tar.gz',
+    title: 'Tomcat 9.0.30 Linux版',
+    content: 'JAVA web 服务器'
+}, {
+    tag: ['java', '服务器'],
+    url: './file/apache-tomcat-8.5.50.tar.gz',
+    title: 'Tomcat 8.5.50 Linux版',
+    content: 'JAVA web 服务器'
+}, {
+    tag: ['流程图', '工具'],
+    url: './file/draw.io-12.1.7-windows-installer.exe',
+    title: 'draw.io',
+    content: '一个开源的功能强大的绘图软件。'
+}, {
+    tag: ['java', 'SDK'],
+    url: './file/jdk-8u51-windows-x64.exe',
+    title: 'jdk8 windows版',
+    content: 'jdk8 windows版',
+}, {
+    tag: ['java', 'SDK'],
+    url: './file/jdk1.8.0_121.zip',
+    title: 'jdk8 Linux版',
+    content: 'jdk8 Linux版',
+}, {
+    tag: ['js'],
+    url: './file/node-v12.10.0-x64.msi',
+    title: 'Node js windows版 64位',
+    content: 'javascript 运行环境。'
+}, {
+    tag: ['web', '工具'],
+    url: './file/Postman-win32-7.0.6-Setup.exe',
+    title: 'Postman windows版 32位',
+    content: '请求测试工具'
+}, {
+    tag: ['java', 'scala', 'SDK'],
+    url: './file/scala-2.11.8.zip',
+    title: 'Scala 2.11.8版',
+    content: 'Scala语言开发环境'
+}, {
+    tag: ['git', '工具'],
+    url: './file/Git-2.23.0-32-bit.exe',
+    title: 'Git客户端 2.23.0 windows版 32位',
+    content: 'Git客户端'
+}, {
+    tag: ['maven', 'java', '工具'],
+    url: './file/apache-maven-3.6.3-bin.zip',
+    title: 'Maven 3.6.3',
+    content: 'java项目依赖版本管理软件'
+}, {
+    tag: ['服务器'],
+    url: './file/nginx-1.17.8.tar.gz',
+    title: 'Nginx 1.17.8 linux版(源代码)',
+    content: `<span>目前性能最强的静态资源代，负载均衡，反向代理服务器</span><br>
+            <span>模块：</span>
+            <a href='./file/pcre-8.43.tar.gz' target='_blank'>pcre</a>&nbsp;
+            <a href='./file/zlib-1.2.11.tar.gz' target='_blank'>zlib</a>&nbsp;
+            <a href='./file/openssl-1.1.1d.tar.gz' target='_blank'>openssl</a><br>
+            <a href='./file/nginx.installer.txt' target='_blank'>编译安装说明参考</a>`
+}, {
+    tag: ['服务器'],
+    url: './file/nginx-1.16.1-1.el7.ngx.x86_64.rpm',
+    title: 'Nginx 1.16.1 linux版(rpm)',
+    content: `<span>目前性能最强的静态资源代，负载均衡，反向代理服务器</span><br>
+            <span>依赖：</span>
+            <a href='./file/openssl-libs-1.0.2k-19.el7.x86_64.rpm' target='_blank'>openssl-libs(rpm)</a>&nbsp;
+            <a href='./file/openssl-1.0.2k-19.el7.x86_64.rpm' target='_blank'>openssl(rpm)</a>`
+}, {
+    tag: ['linux', 'ceotos', '软件', '工具'],
+    url: './file/centos-basic-packages.zip',
+    title: 'CentOS basic packages',
+    content: 'CentOS系统一些基本的软件包，包含c++编译环境,nginx依赖模块等...'
+}, {
+    tag: ['python', 'SDK'],
+    url: './file/python-2.7.17.amd64.msi',
+    title: 'Python 2.7.17 windows版',
+    content: 'Python开发环境'
+}, {
+    tag: ['python', 'SDK'],
+    url: './file/python-3.8.1-amd64.exe',
+    title: 'Python 3.8.1 windows版',
+    content: 'Python开发环境'
+}, {
+    tag: ['编辑器', '工具'],
+    url: './file/typora-setup-x64.exe',
+    title: 'MarkDown文本编辑器',
+    content: '一个开源的所见即所得markdown文本文件编辑器'
+}, {
+    tag: ['编辑器', '工具'],
+    url: './file/VSCodeSetup-x64-1.21.1.exe',
+    title: 'Visual Studio Code文本编辑器',
+    content: `<span>微软一个开源的文本文件编辑器</span><br>
+            <span>扩展：</span>
+            <a href='./file/extensions.zip' target=_blank>extensions</a><span>(C:\\Users\\用户名\\.vscode\\下)</span>`
+}, {
+    tag: ['数据库', '工具'],
+    url: './file/Navicat Premium_11.2.7简体中文版.zip',
+    title: 'Navicat Premium_11.2.7简体中文版',
+    content: 'Navicat Premium_11.2.7简体中文版(带破解程序)'
+}, {
+    tag: ['数据库'],
+    url: './file/redis-5.0.7.tar.gz',
+    title: 'redis 5.0.7 Linux版',
+    content: '一个高性能的内存数据库。'
+}, {
+    tag: ['运行环境', 'SDK'],
+    url: './file/net.exe',
+    title: '.NET Framework 4.5.2运行开发环境',
+    content: '.NET 4.5.2所开发的程序必要的运行环境。'
+}];
